1ce3023214af5b854d15e11d9c50977d4df24916
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2023-12-12 14:25:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function f() {
+	for (let i = 0; i <=20; i++) {
+
+		if (i*3) {
+			console.log("Fizz\n")
+		}else if (i*5) {
+			console.log("Buzz\n")
+		}else if (i*3 && i*5) {
+			console.log("FizzBuzz")
+		}
+	}
+}
+let val=prompt("Enter the number")
+alert(i) 
+f()
